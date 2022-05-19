dfc86941b9e0fca92f6f89a718bd73a270ad9b93
Dennis Brink <dennis.mjknd@gmail.com>
2022-05-20 01:02:28 +0200

    Added a comment line

--- app/src/main/java/com/dennis_brink/android/myerrands/MainActivity.java
+++ app/src/main/java/com/dennis_brink/android/myerrands/MainActivity.java
@@ -61,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupLogo(){
+        // added a comment line for git
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setLogo(R.drawable.ic_logo_padding);
         getSupportActionBar().setTitle(R.string._title);
